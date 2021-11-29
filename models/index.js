7f9e5335cd50c42f325e61ce24c0f2833eb1291e
Granade <mark.granade@gmail.com>
2021-11-28 20:41:31 -0500

    completed routes

--- models/index.js
+++ models/index.js
@@ -1,20 +1,38 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+	foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+	foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+	through: Product,
+	// as: "product_tag",
+	foreignKey: "product_id",
+	// onDelete: 'SET NULL'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+	through: Product,
+	// as: "product_tag",
+	foreignKey: "tag_id",
+	// onDelete: 'SET NULL'
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+	Product,
+	Category,
+	Tag,
+	ProductTag,
 };
